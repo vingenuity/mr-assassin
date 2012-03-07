505a9abba81dd8c472df8390ec042a401e6e38af
sewellka@gmail.com <sewellka@gmail.com@f1dead38-7753-4062-aec7-60e9a071f1b4>
2012-03-07 20:47:41 +0000

    Added basic text to the Target tab

--- MRAssassin/src/com/badideastudios/mrassassin/TargetActivity.java
+++ MRAssassin/src/com/badideastudios/mrassassin/TargetActivity.java
@@ -1,19 +1,35 @@
 package com.badideastudios.mrassassin;
 
+import android.R;
+import android.R.string;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TargetActivity extends Activity {
 
+	public String targetName = "Ezio";
+	public int targetBounty = 500;
 	/** Called when the activity is first created. */
 	@Override
+
 	public void onCreate(Bundle savedInstanceState) 
 	{
 	    super.onCreate(savedInstanceState);
-        TextView view = new TextView(this);
-        view.setText("This will be the Target tab.");
-        setContentView(view);
+	    setContentView(com.badideastudios.mrassassin.R.layout.target);
+	    
+	    TextView name = new TextView(this);
+	    name = (TextView)findViewById(com.badideastudios.mrassassin.R.id.targetName);
+	    name.setText("Current Target: " + targetName);
+	    
+	    TextView currentBounty = new TextView(this);
+	    currentBounty = (TextView)findViewById(com.badideastudios.mrassassin.R.id.targetBounty);
+	    currentBounty.setText("Target Bounty: " + targetBounty);
+	    
+
 	}
 
 }
