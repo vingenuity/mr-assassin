e71b2348f5660aa190189a3155bb1e2f9341d9a9
howlingblue@gmail.com <howlingblue@gmail.com@f1dead38-7753-4062-aec7-60e9a071f1b4>
2012-04-02 22:27:16 +0000

    Added more of the framework for our stats page.

--- MRAssassin/src/com/badideastudios/mrassassin/StatActivity.java
+++ MRAssassin/src/com/badideastudios/mrassassin/StatActivity.java
@@ -3,33 +3,68 @@ package com.badideastudios.mrassassin;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class StatActivity extends Activity 
 {
-	private enum Category { TOP_KILLERS, TOP_MONEY, TOP_BOUNTY };
+	private AssassinApp app;
+	/* Pseudo-enumeration for the statistic types */
+	private final int TOP_KILLERS = 0;
+	private final int TOP_BOUNTY  = 1;
+	private final int TOP_EARNERS = 2;
+	private int currentCategory;
+	private ListView statsList;
 	private TextView categoryText;
-	private Category currentCategory;
-
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) 
 	{
 	    super.onCreate(savedInstanceState);
         setContentView(R.layout.stats);
+        app = (AssassinApp) getApplication();
 
-        currentCategory = Category.TOP_KILLERS;
+        currentCategory = TOP_KILLERS;
+        statsList = (ListView) findViewById(R.id.stat_list_view);
         categoryText = (TextView) findViewById(R.id.category_text);
-        categoryText.setText( currentCategory.name() );
+        refreshPage();
 	}
 	
 	/** Handle left and right button presses*/
-    
     public void left_button(View v)
     {
+    	if(currentCategory != TOP_KILLERS)
+    		currentCategory--;
+    	else
+    		currentCategory = TOP_EARNERS;
+        refreshPage();
     }
     
     public void right_button(View v)
     {
-    	
+    	if(currentCategory != TOP_EARNERS)
+    		currentCategory++;
+    	else
+    		currentCategory = TOP_KILLERS;
+        refreshPage();
+    }
+    
+    private String printTitle(int category)
+    {
+    	switch(category)
+    	{
+    	default:
+    	case TOP_KILLERS:
+    		return "Top Killers:";
+    	case TOP_BOUNTY:
+    		return "Top Bounty Hunters:";
+    	case TOP_EARNERS:
+    		return "Top Earners:";
+    	}
+    }
+    
+    private void refreshPage()
+    {
+        categoryText.setText( printTitle(currentCategory) );
     }
 }
